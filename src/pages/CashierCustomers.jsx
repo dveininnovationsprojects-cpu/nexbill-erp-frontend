fa788b8621d2f4a591f7922aaf623b9dbafb52c2
dveininnovationsprojects-cpu <dveininnovations.projects@gmail.com>
2026-05-25 15:56:12 +0530

    change cashiercustomer

--- src/pages/CashierCustomers.jsx
+++ src/pages/CashierCustomers.jsx
@@ -1,1 +1,7 @@
-export default function CashierCustomers() { return null; }
+import Customers from "./Customers";
+
+function CashierCustomers() {
+  return <Customers role="cashier" />;
+}
+
+export default CashierCustomers;
